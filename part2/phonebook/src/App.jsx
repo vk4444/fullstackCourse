f77d2db09a87a48cf335e9392e754bd15cb3f9b5
Vojtěch Kubala <vojtechkubala@outlook.com>
2025-05-16 21:34:30 +0200

    finished part 2c

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
 import Contacts from './components/Contacts'
 import Search from './components/Search'
+
 
 
 const App = () => {
 
   // STATES
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newPerson, setNewPerson] = useState({name: '', number: '', id: persons.length + 1})
   const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   // FORM HANDLERS
   const handleSubmit = (event) => {
