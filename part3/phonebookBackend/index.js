35a4de7a25eaa670d10bba8c3f9adeaa81fa3a12
Vojtěch Kubala <vojtechkubala@outlook.com>
2025-05-19 20:05:49 +0200

    persons not used in memory anymore

--- part3/phonebookBackend/index.js
+++ part3/phonebookBackend/index.js
@@ -16,61 +16,33 @@ app.use(express.static('dist'))
 app.use(morgan(':method :url :status :response-time :body'))
 app.use(express.json())
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
-app.post('/api/persons', (request, response) => {
-
-  
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   if (!body.name) {
-    return response.status(400).json({ 
-      error: 'name missing' 
-    })
+    return response.status(400).json({ error: 'name missing' })
   }
-
   if (!body.number) {
-    return response.status(400).json({ 
-      error: 'number missing' 
-    })
+    return response.status(400).json({ error: 'number missing' })
   }
 
-  if (persons.map(person => person.name).find(name => name == body.name)) {
-    return response.status(400).json({
-      error: 'the name already exists'
+  Person.findOne({ name: body.name })
+    .then(existingPerson => {
+      if (existingPerson) {
+        return response.status(400).json({ error: 'the name already exists' })
+      }
+
+      const person = new Person({
+        name: body.name,
+        number: body.number,
+      })
+
+      person.save()
+        .then(savedPerson => response.json(savedPerson))
+        .catch(error => next(error))
     })
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-  })
-
-  person.save().then(savedPerson => {
-    persons.concat(savedPerson)
-    response.json(savedPerson)
-  })
+    .catch(error => next(error))
 })
 
 app.get('/', (request, response) => {
